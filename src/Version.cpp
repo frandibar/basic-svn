54c15bd8fcd54a89c46ae05a9c6f82473a19a2ff
rodrigo.bengoechea <rodrigo.bengoechea@d098c9a8-a62e-0410-89ac-cf5208c6bbc1>
2007-06-02 03:41:19 +0000

    Tested Bloque Version & VersionFile

--- src/Version.cpp
+++ src/Version.cpp
@@ -6,13 +6,12 @@ Version::Version()
 {
     _nroVersion = -1;
     _original   = -1;
-    _fecha      = -1;
     _tipo       = 0;
     _offset     = -1;
     _user       = 0;
 }
 
-Version::Version(int NroVersion, int Original, time_t Fecha, const char* User, long int Offset, char Tipo)
+Version::Version(int NroVersion, int Original, tm Fecha, const char* User, long int Offset, char Tipo)
 {
     _nroVersion = NroVersion;
     _original = Original;
@@ -30,11 +29,13 @@ Version::Version(int NroVersion, int Original, time_t Fecha, const char* User, l
 
 Version::~Version()
 {
-    delete _user;
+	if(_user);
+		delete _user;
 }
 
 void Version::write(char* buffer)
 {
+	int writeen = 0;
     int tamanioUsuario;
     //copio el nro de version
     memcpy(buffer,&_nroVersion,sizeof(int));
@@ -45,12 +46,12 @@ void Version::write(char* buffer)
     buffer += sizeof(int);
 
     //copio la fecha de la version
-    memcpy(buffer,&_fecha,sizeof(time_t));
-    buffer += sizeof(time_t);
+    memcpy(buffer,&_fecha,sizeof(tm));
+    buffer += sizeof(tm);
 
     //copio el offset dentro del archivo de originales y diffs
-    memcpy(buffer,&_offset,sizeof(int));
-    buffer += sizeof(int);
+    memcpy(buffer,&_offset,sizeof(long int));
+    buffer += sizeof(long int);
 
     //copio el tamaño del campo usuario
     tamanioUsuario = strlen(_user);
@@ -68,52 +69,53 @@ void Version::write(char* buffer)
     return;
 }
 
-void Version::read(char* buffer)
+void Version::read(char** buffer)
 {
     int tamanioUsuario;
     // leo el nro de version
-    memcpy(&_nroVersion,buffer,sizeof(int));
-    buffer += sizeof(int);
+    memcpy(&_nroVersion,*buffer,sizeof(int));
+    *buffer += sizeof(int);
 
     //leo el nº de version original
-    memcpy(&_original,buffer,sizeof(int));
-    buffer += sizeof(int);
+    memcpy(&_original,*buffer,sizeof(int));
+    *buffer += sizeof(int);
 
     //leo la fecha de la version
-    memcpy(&_fecha, buffer, sizeof(time_t));
-    buffer += sizeof(time_t);
+    memcpy(&_fecha, *buffer, sizeof(tm));
+    *buffer += sizeof(tm);
 
     //leo el offset de la version dentro del archivo de originales y diffs
-    memcpy(&_offset,buffer,sizeof(int));
-    buffer+= sizeof(int);
+    memcpy(&_offset,*buffer,sizeof(int));
+    *buffer+= sizeof(int);
 
     //leo el usuario, previamente leo la longitud de este campo
-    memcpy(&tamanioUsuario,buffer,sizeof(int));
-    buffer += sizeof(int);
+    memcpy(&tamanioUsuario,*buffer,sizeof(int));
+    *buffer += sizeof(int);
 
     if(_user != 0){
         delete _user;
         _user = 0;
     }
     _user = new char[(tamanioUsuario + 1)*sizeof(char)];    // creo el campo
-    memcpy(_user,buffer,tamanioUsuario * sizeof(char)); // copio los caracteres
+    memcpy(_user,*buffer,tamanioUsuario * sizeof(char)); // copio los caracteres
     _user[tamanioUsuario] = 0;  // coloco la marca de fin
-    buffer += tamanioUsuario * sizeof(char);
+    *buffer += tamanioUsuario * sizeof(char);
 
     //leo el tipo de archivo
-    memcpy(&_tipo,buffer,sizeof(char));
-    buffer += sizeof(char);
+    memcpy(&_tipo,*buffer,sizeof(char));
+    *buffer += sizeof(char);
 
     return;
 }
 
 int Version::tamanioEnDisco(){
-    int tamanio = sizeof(int) +                   //nro de version 
-                  sizeof(int) +                   //nro del original
-                  sizeof(long int) +              //fecha
-                  sizeof(int) +                   //longitud del atributo nombre usuario
-                  strlen(_user) * sizeof(char) +  //usuario
-                  sizeof(char);                   //tipo
+    int tamanio = sizeof(int);					//nro de version 
+		tamanio+= sizeof(int);					//nro del original                  
+        tamanio+= sizeof(tm);					//fecha
+		tamanio+= sizeof(long int);				//offset				  
+		tamanio+= sizeof(int);					//longitud del atributo nombre usuario				  
+        tamanio+= strlen(_user) * sizeof(char);	//usuario                   
+        tamanio+= sizeof(char);					//tipo                                     
 
     return tamanio;
 }
--- src/Version.h
+++ src/Version.h
@@ -12,17 +12,17 @@ class Version
 {
 public:
     Version();
-    Version(int NroVersion, int Original, time_t Fecha, const char* User, long int Offset, char Tipo);
+    Version(int NroVersion, int Original, tm Fecha, const char* User, long int Offset, char Tipo);
     ~Version();
 
     //empaquetadores y desempaquetadores
-    void read (char* buffer);
+    void read (char** buffer);
     void write(char* buffer);
 
     //getters - no hay setters porque no hay modificacion de la info de las versiones
     int      getNroVersion()  const { return _nroVersion; }
     int      getOriginal()    const { return _original;   }
-    time_t   getFecha()       const { return _fecha;      }
+    tm		 getFecha()       const { return _fecha;      }
     char*    getUser()        const { return _user;       }
     long int getOffset()      const { return _offset;     }
     char     getTipo()        const { return _tipo;       }
@@ -37,7 +37,7 @@ protected:
     
     int         _nroVersion; // numero de la version        
     int         _original;   // numero de la version que es el ultimo original de este archivo
-    time_t      _fecha;      // fecha en que se establecio la version
+    tm			_fecha;      // fecha en que se establecio la version
     char*       _user;       // el usuario que establecio la version
     long int    _offset;     // offset donde se encuentra la version en el archivo que contiene originales y diffs
     char        _tipo;       // tipo de archivo del que se trata la version "b" = binario, "t" = texto
