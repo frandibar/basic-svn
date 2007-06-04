32222102b0033737386da8b9d155e3d56822919d
frandibar <frandibar@d098c9a8-a62e-0410-89ac-cf5208c6bbc1>
2007-06-04 03:37:09 +0000

    svnadmin complete

--- src/nodobmas.h
+++ src/nodobmas.h
@@ -5,14 +5,12 @@
 #include <string>
 #include <iostream>
 
-#define TAMANIOARREGLO 102
+#define TAMANIOARREGLO 2022   // TAMANIONODO - bytes admin bloque (_id + _nivel + _padre + _nclaves + _espacioLibre)
 
 class NodoBMas 
 {
 public:
-    static const int LONGCLAVE;
 //    static const int TAMANIOARREGLO;
-    static const int MAXCLAVES;
     static const int TAMANIONODO;
     static const int NODOHOJA;
     static const int NODOINDICE;
@@ -30,7 +28,7 @@ public:
     // - -1 si la clave buscada no esta en el nodo y es un nodo hoja.
     // - la referencia al nodo siguiente donde buscar si el nodo es indice
     virtual int searchFile(const char* key) = 0; // key = nombre del archivo a buscar
-	virtual int searchFileAndVersion(const char* fileName,int version) = 0;
+    virtual int searchFileAndVersion(const char* fileName,int version) = 0;
     
                                                                         
     // metodo virtual para insertar una clave en un nodo
@@ -43,8 +41,8 @@ public:
     int getId()         const { return _id;      }
     int getPadre()      const { return _padre;   }
     int getCantClaves() const { return _nclaves; }
-	int getOffset()		const { return _offset;  }
-	int getNivel()		const { return _nivel;	 }
+    int getOffset()     const { return _offset;  }
+    int getNivel()      const { return _nivel;   }
     int getRef(int idNode);
     // devuelve 1 si el nodo es interior y 2 si es hoja
     virtual int getType() const = 0;
@@ -54,20 +52,20 @@ public:
 
     // generador de un nodo del mismo _nivel para cuando tengo que hacer un split
     virtual NodoBMas* split(int Numero, char* arreglo,int bytesArreglo,int clavesArreglo,
-							char** claveAlPadre) = 0;
+                            char** claveAlPadre) = 0;
 
     // me genera 2 nuevos nodos al tener que promover la raiz
     virtual void promoteRoot(NodoBMas** nodo1, NodoBMas** nodo2,int id1,int id2,
-							int clavesArreglo, char** arreglo, int bytesArreglo,
-							char** claveARaiz)  = 0;
+                            int clavesArreglo, char** arreglo, int bytesArreglo,
+                            char** claveARaiz)  = 0;
 
 protected:
     int insertPair(const char* key, int ref);
-    int insertPair(	const char* key, int ref, int* offset,
-					int* clavesArregloAux,char** arregloAux);
-	int insertPair(const char* key,int ref,int* clavesArreglo,char** arregloAux,int* bystesArreglo);
+    int insertPair( const char* key, int ref, int* offset,
+                    int* clavesArregloAux,char** arregloAux);
+    int insertPair(const char* key,int ref,int* clavesArreglo,char** arregloAux,int* bystesArreglo);
     void completePairs(int* offset,int tamanioArreglo,char* arregloAux);
-	void completePairs(int* tamanioArreglo,char** arregloAux,int* bytesArreglo);            
+    void completePairs(int* tamanioArreglo,char** arregloAux,int* bytesArreglo);            
 
     void readInfoAdm (char** nextByte);
     void readDatos   (char** nextByte);
@@ -75,14 +73,14 @@ protected:
     void writeDatos  (char** nextByte);
 
     // member variables
-    int       _id;			// id del nodo
-    int       _nivel;		// nivel del nodo
-    int       _padre;		// referencia al padre del nodo
-    short int _nclaves;		// cantidad de claves que contiene el nodo
-    char _pares[TAMANIOARREGLO]; //char*      _pares;		// pares clave-referencia en el caso de los nodos hoja
-							// pares clave-hijo claves mayores en el caso de los interiores
-	int		_espacioLibre;	// cantidad de espacio libre dentro del nodo
-	int		_offset;		// el offset de donde voy a empezar a insertar, esta variable se calcula
+    int       _id;               // id del nodo
+    int       _nivel;            // nivel del nodo
+    int       _padre;            // referencia al padre del nodo
+    short int _nclaves;          // cantidad de claves que contiene el nodo
+    int       _espacioLibre;     // cantidad de espacio libre dentro del nodo
+    int       _offset;           // el offset de donde voy a empezar a insertar, esta variable se calcula
+    char _pares[TAMANIOARREGLO]; // pares clave-referencia en el caso de los nodos hoja
+                                 // pares clave-hijo claves mayores en el caso de los interiores
 
 };                       
     
